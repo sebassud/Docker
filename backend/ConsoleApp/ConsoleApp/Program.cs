b295b7d48398fee858f3acf69fa4a81d2da8a85b
Sebastian Sudra <sebastian.sudra@pentacomp.pl>
2021-07-22 20:23:37 +0200

    kubernetes konsola

---
+++ backend/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private static int iterator = 0;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Rozpoczynam liczenie.");
+            var timer = new System.Timers.Timer(2000);
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+
+            while(true) { }
+        }
+
+        private static void OnTimedEvent(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            Console.WriteLine($"Iterator: {iterator}");
+            iterator++;
+        }
+    }
+}
